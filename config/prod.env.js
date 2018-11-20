8ded5016bf1ddf4b9c9d8d2377a9d009c9cd0840
hyf <459909293@qq.com>
2018-11-20 10:59:23 +0800

    hyf

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ASSETS_PUBLIC_PATH: '"https://staticcdn.fantuan.cn/"',
+  ASSETS_PUBLIC_PATH: '"https://staticcdn.fantuan.cn/pch5/"',
   API_DOMAIN: '"http://fant.fantuan.cn"',
   API_DOMAIN_HTTPS: '"https://fant.fantuan.cn"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"test"',
-  ASSETS_PUBLIC_PATH: '"https://staticcdntest.fantuan.cn/"',
+  ASSETS_PUBLIC_PATH: '"https://staticcdntest.fantuan.cn/pch5/"',
   API_DOMAIN: '"http://fanttest.fantuan.cn"',
   API_DOMAIN_HTTPS: '"https://fanttest.fantuan.cn"'
 })
