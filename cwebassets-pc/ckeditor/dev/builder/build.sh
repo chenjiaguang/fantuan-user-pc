252fba2bd43d9cf49fdb83b6e1a769753a16a300
hyf <459909293@qq.com>
2018-11-20 14:52:45 +0800

    hyf editor加入cdn

--- cwebassets-pc/ckeditor/dev/builder/build.sh
+++ cwebassets-pc/ckeditor/dev/builder/build.sh
@@ -69,8 +69,10 @@ if [ ${#TAG} -le 0 ];
 then
 	VERSION="$VERSION DEV"
 fi
-
-java -jar ckbuilder/$CKBUILDER_VERSION/ckbuilder.jar --build ../../ release $JAVA_ARGS --version="$VERSION" --revision="$REVISION" --overwrite
+echo "##########"
+RAND=$(date +%s%N)
+rm -rf ./release*
+java -jar ckbuilder/$CKBUILDER_VERSION/ckbuilder.jar --build ../../ release$RAND $JAVA_ARGS --version="$VERSION" --revision="$REVISION" --overwrite
 
 # Copy and build tests.
 if [[ "$ARGS" == *\ \-t\ * ]]; then
